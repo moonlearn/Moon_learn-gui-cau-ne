34794dad0bc8e698e98ed83b1267f0cf0b89657c
moonlearn <106872702+moonlearn@users.noreply.github.com>
2022-06-18 14:33:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,15 @@
 const CONFIG = {
     introTitle: 'Gửi bạn dễ thương✨',
     introDesc: `Bạn có thư tình từ Moon.learn nè 🥰`,
-    title: 'Bạn có muốn đi date với Moon qua buổi workshop miễn phí này khum? 😙',
-    desc: 'Thử thách 2 tiếng trở nên tự tin hơn, gét gô!!! ',
+    title: 'Bạn có muốn đi date với Moon qua 3 tuần bứt phá English Speaking khum? 😙',
+    desc: 'Thử thách 21 ngày trở nên phiên bản tự tin hơn của mình, gét gô!!! ',
     btnYes: 'Có nhaaaa🥰',
     btnNo: 'Khum, cảm ơn',
     question:'Bạn có tâm tư tình cảm gì gửi cho Moon.learn hem? 🤧',
     btnReply: 'Gửi cho Moon <3',
-    reply: 'Mình muốn tham gia tại tò mò về workshop, cũng muốn nâng cấp bản thân và trở nên tự tin hơn ó',
+    reply: 'Mình muốn tham gia tại tò mò về chương trình, cũng muốn nâng cấp bản thân và trở nên tự tin nói Tiếng Anh hơn ó',
     mess: 'Ỏ Moon.learn bắt đầu crush cậu rùi đó, phải làm sao phải làm sao 😘',
-    messDesc: 'Link form đăng ký giữ chỗ ở đây nha: forms.gle/nJBky6bTfRB3DdSi9',
+    messDesc: 'Link form đăng ký giữ chỗ ở đây nha: https://forms.gle/z2bvMp65Fwdo3wJb7',
     btnAccept: 'Okiiiii mình đăng ký liền nè <3',
     messLink: 'https://www.instagram.com/moon.learn/'
 }
